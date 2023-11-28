96a7c7a7922034d749b046b0cccf3b0bcd887059
bodeych <bodeych@gmail.com>
2023-11-28 22:42:10 +0100

    easy logic for calc

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,3 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+double firstValue, secondValue;
+string action;
 
+Console.Write("First number: ");
+firstValue = double.Parse(Console.ReadLine());
+
+Console.Write("Second number: ");
+secondValue = double.Parse(Console.ReadLine());
+
+Console.Write("Select operation: '+', '-', '*', '/'");
+action = Console.ReadLine();
+
+switch (action)
+{
+    case "+":
+        Console.WriteLine(firstValue + secondValue);
+        break;
+    case "-":
+        Console.WriteLine(firstValue - secondValue);
+        break;
+    case "*":
+        Console.WriteLine(firstValue * secondValue);
+        break;
+    case "/":
+        if (secondValue == 0)
+        {
+            Console.WriteLine(0);
+        }
+        else
+        {
+            Console.WriteLine(firstValue / secondValue);
+        }
+            break;
+
+    default:
+        Console.WriteLine("Error");
+        break;
+}
+Console.ReadLine();
